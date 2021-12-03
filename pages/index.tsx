cb993c378d5e336fc0085fb04d472f5eacdd2e98
Solex <solomonyunana95@gmail.com>
2021-12-03 14:57:09 +0100

    Intialized nextjs app and configured home screen

---
+++ pages/index.tsx
@@ -0,0 +1,11 @@
+import type { NextPage } from "next";
+import Head from "next/head";
+import Image from "next/image";
+import styles from "../styles/Home.module.css";
+import Layout from "../components/layout";
+
+const Home: NextPage = () => {
+  return <Layout></Layout>;
+};
+
+export default Home;
